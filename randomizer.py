b1840fb287a578e9a92ee73d50c9a02bbb14494c
0xAymane <91842247+0xAymane@users.noreply.github.com>
2021-10-04 01:41:11 +0200

    Create randomizer.py

---
+++ randomizer.py
@@ -0,0 +1,13 @@
+import random
+
+randList = []
+for i in range(1000):
+	randList.append(i+1)
+random.shuffle(randList)
+
+for i in range(1000):
+	with open("jsons/" + str(i)) as f:
+		data = json.load(f)
+
+	with open("jsonsNew/" + str(randList[i]), 'w') as f:
+		json.dump(data, f, indent=2)
